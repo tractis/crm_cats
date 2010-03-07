b6a0c21f0335310be7ca91157d934ea92ebf1895
jose-gordo <jose.gordo@tractis.com>
2010-03-07 11:12:22 +0100

    cats filter links labels change: if in own model ajax filter, if in other context (ex. viewing contacts from account view) link to with query string

--- lib/crm_cats/crm_cats_view_helpers.rb
+++ lib/crm_cats/crm_cats_view_helpers.rb
@@ -9,9 +9,14 @@ module CrmCats
   
     # Generate cat links for use on asset index pages.
     #----------------------------------------------------------------------------
-    def cats_for_index(model)
+    def cats_for_index(model)    
       model.cats.inject([]) do |arr, cat|
-        arr << link_to_function(cat.long_name, "crm.search_categorized('$#{cat.id}', '#{model.class.to_s.tableize}')", :title => cat.description)
+        # Ajax category filter if viewed from same controller (not from other asset, ex. viewing contacts from account view)
+        if model.class.to_s.downcase.pluralize == controller.controller_name.to_s
+          arr << link_to_function(cat.long_name, "crm.search_categorized('$#{cat.id}', '#{model.class.to_s.tableize}')", :title => cat.description)
+        else
+          arr << link_to(cat.long_name, { :controller => model.class.to_s.downcase.pluralize, :query => "$#{cat.id}" }, { :title => cat.description })
+        end
       end.join(" ")
     end
 
